a966196974b2600ac4cbb0573cd516c0af2ae1cd
Kavi-200312 <cskkavi2003@gmail.com>
2025-04-17 23:21:46 +0530

    dddddvd

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -4,7 +4,7 @@ import { ProtectRoute  } from "../middleware/auth.middleware.js"
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controllers.js"
 const messageRouter = express.Router()
 
-messageRouter.get("/:id" , ProtectRoute  ,getMessages)
 messageRouter.get("/users" , ProtectRoute  ,getUsersForSidebar)
+messageRouter.get("/:id" , ProtectRoute  ,getMessages)
 messageRouter.post("/send/:id" , ProtectRoute  ,sendMessage)
 export default messageRouter
